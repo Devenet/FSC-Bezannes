246f9cb6d908007e185708cf593923aaac39def0
nicolabricot <github@nicolabricot.com>
2012-12-21 03:01:23 +0100

    Add sql connection

---
+++ lib/db/sql.php
@@ -0,0 +1,19 @@
+<?php
+namespace lib\db;
+use PDO;
+
+class SQL {
+    private static $instance;
+
+    static public function sql(){
+        if(!isset(self::$instance)){
+            self::$instance = new PDO('mysql:host=localhost;dbname=foyer', 'foyer', 'foyer');
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        }
+        self::$instance->query('SET NAMES UTF8');
+        return self::$instance;
+    }
+}
+
+?>
