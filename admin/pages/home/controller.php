c8c64fc9e33f430a5522ad6911b250f070b25501
nicolabricot <github@nicolabricot.com>
2012-12-23 14:24:12 +0100

    Replace infos into controller/view

--- admin/pages/home/controller.php
+++ admin/pages/home/controller.php
@@ -1,12 +1,15 @@
 <?php
 
 use lib\content\Page;
+use lib\activities\Activity;
 
 $pageInfos = array(
   'name' => 'Administration',
   'url' => '/'
 );
 $page = new Page($pageInfos['name'], $pageInfos['url'], array());
+
+$activities = Activity::countActivities();
 
 
 ?>
