4648de256de72acd2b0e2ca41f3a19ff9d249d4e
nicolabricot <github@nicolabricot.com>
2012-12-26 23:48:27 +0100

    Indent text and change icons

--- admin/pages/activities/view.php
+++ admin/pages/activities/view.php
@@ -7,7 +7,7 @@
     <tr>
       <th>#</th>
       <th>Activité</th>
-      <th>Visible</th>
+      <th>Activée</th>
       <th>Tarif</th>
       <th>Tarif jeune</th>
       <th> </th>
@@ -17,8 +17,8 @@
     <?php foreach ($activities as $act): ?>
     <tr <?php echo $act->active() == 0 ? 'class="muted"' : null; ?>>
       <td><?php echo $act->id(); ?></td>
-      <td><a href="/?page=activity&id=<?php echo $act->id(); ?>"><?php echo $act->name(); ?></a></td>
-      <td><?php echo ($act->active() == 1) ? '<a href="'. _FSC_ .'/?page=activite&id='. $act->id() .'" target="_blank"><i class="icon-ok"></i></a>' : '<i class="icon-ban-circle"></i>' ; ?></td>
+      <td><a href="/?page=activity&amp;id=<?php echo $act->id(); ?>"><?php echo $act->name(); ?></a></td>
+      <td><?php echo ($act->active() == 1) ? '<a href="'. _FSC_ .'/?page=activite&amp;id='. $act->id() .'" target="_blank"><i class="icon-ok"></i></a>' : '<i class="icon-ban-circle"></i>' ; ?></td>
       <td><?php echo $act->price(); ?> €</td>
       <td><?php echo ($act->price_young() == -1) ? '&ndash;' : $act->price_young() .' €'; ?></td>
       <td style="width: 80px;">
@@ -27,8 +27,8 @@
           <span class="caret"></span>
           </a>
           <ul class="dropdown-menu">
-            <li><a href="/?page=activity&id=<?php echo $act->id(); ?>"><i class="icon-eye-open"></i> Voir</a></li>
-            <li><a href="/?page=edit-activity&id=<?php echo $act->id(); ?>"><i class="icon-pencil"></i> Modifier</a></li>
+            <li><a href="/?page=activity&amp;id=<?php echo $act->id(); ?>"><i class="icon-eye-open"></i> Voir</a></li>
+            <li><a href="/?page=edit-activity&amp;id=<?php echo $act->id(); ?>"><i class="icon-pencil"></i> Modifier</a></li>
             <li class="divider"></li>
             <li><a href="#confirmBox<?php echo $act->id(); ?>" role="button" data-toggle="modal"><i class="icon-trash"></i> Supprimer</a></li>
           </ul>
@@ -41,16 +41,16 @@
 
 <?php foreach ($activities as $act): ?>
 <div id="confirmBox<?php echo $act->id(); ?>" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="ConfirmDelActivity<?php echo $act->id(); ?>" aria-hidden="true">
-<div class="modal-header">
-<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-<h3 id="ConfirmDelActivity<?php echo $act->id(); ?>"><?php echo $act->name(); ?></h3>
-</div>
-<div class="modal-body">
-<p class="text-error">Êtes-vous sûr de vouloir supprimer cette activité ?</p>
-</div>
-<div class="modal-footer">
-<a class="btn" data-dismiss="modal" aria-hidden="true"/>Annuler</a>
-<a href="/?page=activity&id=<?php echo $act->id(); ?>&action=delete" class="btn btn-danger">Confirmer</a>
-</div>
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+    <h3 id="ConfirmDelActivity<?php echo $act->id(); ?>"><?php echo $act->name(); ?></h3>
+  </div>
+  <div class="modal-body">
+    <p class="text-error">Êtes-vous sûr de vouloir supprimer cette activité ?</p>
+  </div>
+  <div class="modal-footer">
+    <a class="btn" data-dismiss="modal" aria-hidden="true"/>Annuler</a>
+    <a href="/?page=activity&amp;id=<?php echo $act->id(); ?>&amp;action=delete" class="btn btn-danger">Confirmer</a>
+  </div>
 </div>
 <?php endforeach; ?>
