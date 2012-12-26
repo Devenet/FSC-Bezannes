f52f622f19c330caa66751345f041f001776e3db
nicolabricot <github@nicolabricot.com>
2012-12-26 23:50:43 +0100

    Add plural functions

--- admin/pages/home/controller.php
+++ admin/pages/home/controller.php
@@ -2,6 +2,7 @@
 
 use lib\content\Page;
 use lib\activities\Activity;
+use lib\members\Member;
 
 $pageInfos = array(
   'name' => 'Administration',
@@ -10,6 +11,9 @@ $pageInfos = array(
 $page = new Page($pageInfos['name'], $pageInfos['url'], array());
 
 $activities = Activity::countActivities();
+$plural_activities = ($activities > 1 ? 's' : null);
 
+$members = Member::countMembers();
+$plural_members = ($members > 1 ? 's' : null);
 
 ?>
