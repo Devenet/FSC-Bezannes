aea095d4a1ecab7f4b8dfe8caa712d9bfcc6ba35
nicolabricot <github@nicolabricot.com>
2012-12-26 23:51:16 +0100

    Add year and season constants

--- config/config.php
+++ config/config.php
@@ -1,8 +1,12 @@
 <?php
 
   define('_FSC_', 'http://fsc.localhost.local');
-  define('_ADMIN_', 'http://admin.fsc.localhost.local');
+  define('_ADMIN_', 'http://gestion.fsc.localhost.local');
+  
   //define('_JQUERY_', 'http://code.jquery.com/jquery-latest.js');
   define('_JQUERY_', 'http://api.localhost.local/jquery/jquery-1.8.0.min.js');
+  
+  define('_YEAR_', 2012);
+  defin('_SEASON_', '2012-2013');
   
   ?>
