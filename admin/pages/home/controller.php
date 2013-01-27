f93e2fd8a3f1d901b310ed722f70c0754489cb98
nicolabricot <github@nicolabricot.com>
2013-01-27 18:39:34 +0100

    Add some tabs for infos in the main admin page

--- admin/pages/home/controller.php
+++ admin/pages/home/controller.php
@@ -3,6 +3,8 @@
 use lib\content\Page;
 use lib\activities\Activity;
 use lib\members\Member;
+use lib\users\UserAdmin;
+use lib\content\Display;
 
 $pageInfos = array(
   'name' => 'Administration',
@@ -22,5 +24,15 @@ $plural_members = ($members > 1 ? 's' : null);
 $adherents = Member::countAdherents();
 $plural_adherents = ($adherents > 1 ? 's' : null);
 
+$configs = array ('_FSC_', '_ADMIN_', '_INSCRIPTION_', '_ANALYTICS_FSC_', '_ANALYTICS_ADMIN_', '_ANALYTICS_INSCRIPTION_', '_JQUERY_', '_YEAR_', '_SEARCH_ENGINE_', '_PHONE_SEC_');
+$display_constants = '';
+foreach ($configs as $c)
+    $display_constants .= '<tr style="font-family: monospace;"><td><code>'. $c .'</code></td><td>'. (is_bool(constant($c)) ? (constant($c) ? '<span style="color:rgb(70, 136, 71);">true</span>' : '<span style="color:rgb(185, 74, 72);">false</span>') : constant($c)) .'</td></tr>';
+
+$history = UserAdmin::getHistory();
+$display_history = '<tbody>';
+foreach ($history as $data) 
+  $display_history .= '<tr><td>' . Display::FullTimestampDate($data['date']) . '</td><td>'. Display::FullTimestampHour($data['date']) .'</td><td>'. $data['name'] . ' <span class="pull-right"><a href="mailto:'. $data['email'] .'" title="Envoyer un courriel"><i class="icon-envelope"></i></a></span></td><td><code>'. Display::Privilege($data['privilege']) .'</code></td><td>'. $data['ip'] . '</td></tr>';
+$display_history .= '</tbody>';
 
 ?>
--- admin/pages/home/view.php
+++ admin/pages/home/view.php
@@ -1,14 +1,15 @@
 <div class="tabbable">
   <ul class="nav nav-tabs">
-    <li class="active"><a href="#tab1" data-toggle="tab"><i class="icon-globe"></i> Activités</a></li>
-    <li><a href="#tab2" data-toggle="tab"><i class="icon-user"></i> Membres</a></li>
-    <li><a href="#tab3" data-toggle="tab"><i class="icon-list-alt"></i> Préinscriptions</a></li>
-    <li class="pull-right"><a href="#tab4" data-toggle="tab"><i class="icon-cog"></i> Configuration</a></li>
+    <li class="active"><a href="#tab-activities" data-toggle="tab"><i class="icon-globe"></i> Activités</a></li>
+    <li><a href="#tab-members" data-toggle="tab"><i class="icon-user"></i> Membres</a></li>
+    <li><a href="#tab-preinscriptions" data-toggle="tab"><i class="icon-list-alt"></i> Préinscriptions</a></li>
+    <li class="pull-right"><a href="#tab-history" data-toggle="tab"><i class="icon-list-alt"></i> Historique</a></li>
+    <li class="pull-right"><a href="#tab-config" data-toggle="tab"><i class="icon-cog"></i> Configuration</a></li>
   </ul>
   
   <div class="tab-content">
     <!-- activites -->
-    <div class="tab-pane active" id="tab1">
+    <div class="tab-pane active" id="tab-activities">
       <div class="btn-group">
         <a href="/?page=activities" class="btn"><i class="icon-eye-open"></i> Voir</a>
         <a href="/?page=new-activity" class="btn"><i class="icon-plus"></i> Ajouter</a>
@@ -19,7 +20,7 @@
     </div>
     
     <!-- membres -->
-    <div class="tab-pane" id="tab2">
+    <div class="tab-pane" id="tab-members">
       <div class="btn-group">
         <a href="/?page=members" class="btn"><i class="icon-eye-open"></i> Voir</a>
         <a href="/?page=new-member" class="btn"><i class="icon-plus"></i> Ajouter</a>
@@ -30,48 +31,44 @@
     </div>
     
     <!-- preinscriptions -->
-    <div class="tab-pane" id="tab3">
+    <div class="tab-pane" id="tab-preinscriptions">
       <p><a href="<?php echo _INSCRIPTION_; ?>" class="btn">Coming soon...</a></p>
     </div>
     
+    <!-- history -->
+    <div class="tab-pane" id="tab-history">
+      <h3>Dernières connexions</h3>
+      <div class="row">
+        <div class="span10 offset1">
+          <table class="table table-striped table-bordered table-hover">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Heure</th>
+                <th>Utilisateur</th>
+                <th>Privilèges</th>
+                <th>Adresse IP</th>
+              </tr>
+            </thead>
+            <?php echo $display_history; ?>
+          </table>
+        </div>
+      </div>
+    </div>
+    
     <!-- config -->
-    <div class="tab-pane" id="tab4">
-      <p>Coming later...</p>
+    <div class="tab-pane" id="tab-config">
+      <h4>debug</h4>
+      <ul>
+        <li><a href="https://docs.google.com/spreadsheet/ccc?key=0AtVq0USuJNKUdGttRUlhSjd6M1FLV0tTTFo3LVd2X3c" target="_blank">Suivi des bogues</a> [Google Doc]</li>
+        <li><a href="/db.php" target="_blank">Database</a></li>
+      </ul>
+      <h4 style="margin-top:30px;">constants</h4>
+      <table class="table table-striped">
+        <?php echo $display_constants; ?>
+      </table>
+      
     </div>
     
   </div>
 </div>
-
-<!--
-<div class="row">
-  
-  <div class="span2 offset3">
-    <div class="btn-group">
-      <a class="btn dropdown-toggle btn-large" data-toggle="dropdown" href="#"><i class="icon-globe"></i>
-      Activités
-      <span class="caret"></span>
-      </a>
-      <ul class="dropdown-menu">
-        <li><a href="/?page=activities"><i class="icon-eye-open"></i> Voir</a></li>
-        <li class="divider"></li>
-        <li><a href="/?page=new-activity"><i class="icon-plus"></i> Ajouter</a></li>
-      </ul>
-    </div>
-  </div>
-  
-  <div class="span2 offset2">
-    <div class="btn-group">
-      <a class="btn dropdown-toggle btn-large" data-toggle="dropdown" href="#"><i class="icon-user"></i>
-      Membres
-      <span class="caret"></span>
-      </a>
-      <ul class="dropdown-menu">
-        <li><a href="/?page=members"><i class="icon-eye-open"></i> Voir</a></li>
-        <li class="divider"></li>
-        <li><a href="/?page=new-member"><i class="icon-plus"></i> Ajouter</a></li>
-      </ul>
-    </div>
-  </div>
-  
-</div>
--->
