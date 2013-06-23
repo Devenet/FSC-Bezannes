f275d6c3486ec535d782c1442d9b843ced460594
nicolabricot <github@nicolabricot.com>
2013-06-23 13:04:35 +0200

    Update document errors
    Add OVH configuration PHP5

--- inscription/index.php
+++ inscription/index.php
@@ -26,7 +26,7 @@ $_GET['page'] = str_replace("\0", '', $_GET['page']);
 $_GET['page'] = str_replace(DIRECTORY_SEPARATOR, '', $_GET['page']);
 
 $_SCRIPT = array();
-$controller = ($_GET['page'] == 404 || $_GET['page'] == 403) ? '../pages/errors/'.$_GET['page'].'/controller.php' : '../'._PATH_INSCRIPTION_.'/pages/'.$_GET['page'].'/controller.php';
+$controller = ($_GET['page'] == 404 || $_GET['page'] == 403) ? '../'. _PATH_PUBLIC_ .'/pages/errors/'.$_GET['page'].'/controller.php' : '../'._PATH_INSCRIPTION_.'/pages/'.$_GET['page'].'/controller.php';
 $controller = file_exists($controller) ? $controller : '../'. _PATH_PUBLIC_ .'/pages/errors/404/controller.php';
 require_once _PATH_INSCRIPTION_. DIRECTORY_SEPARATOR .$controller;
 
