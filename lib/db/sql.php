3cf2821ed7ec2e3f98cca9639fd5135fff747216
nicolabricot <github@devenet.info>
2013-06-25 20:35:20 +0200

    Add database configuration in a json file

--- lib/db/sql.php
+++ lib/db/sql.php
@@ -4,17 +4,30 @@ use PDO;
 
 class SQL {
     private static $instance;
+    private static $access = 0;
+    private static $file = '../config/database.json';
 
     static public function sql(){
-        if(!isset(self::$instance)){
-            //self::$instance = new PDO('mysql:host=mysql51-40.perso;dbname=bezannesfsc', 'bezannesfsc', 'Zdu32xdZ');
-            self::$instance = new PDO('mysql:host=localhost;dbname=foyer', 'foyer', 'foyer');
+        if (!isset(self::$instance)){
             
-            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            if (file_exists(self::$file)) {
+                $db = json_decode(file_get_contents(self::$file));
+                
+                self::$instance = new PDO($db->dsn, $db->login, $db->foyer);
+            
+                self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            }
+            else
+                throw new \Exception('DataBase file configuration not found');
         }
-        self::$instance->query('SET NAMES UTF8');
+        //self::$instance->query('SET NAMES UTF8');
+        self::$access++;
         return self::$instance;
+    }
+
+    static public function access() {
+        return self::$access;
     }
 }
 
