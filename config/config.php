cffc9c0ecd6a4c6ed125a50ff6989be47ff73340
nicolabricot <github@devenet.info>
2013-07-14 23:56:27 +0200

    Update config PATHs

--- config/config.php
+++ config/config.php
@@ -4,13 +4,13 @@
   define('_PATH_GESTION_', 'gestion');
   define('_PATH_ADMIN_', 'admin');
   define('_PATH_PREINSCRIPTION_', 'preinscription');
-  define('_PATH_STATIC_', _PATH_PUBLIC_);
-  define('_PATH_UPLOADS_', _PATH_STATIC_.'/'.'uploads');
+  define('_PATH_UPLOADS_', 'uploads');
+  define('_PATH_UPLOADS_FULL_', _PATH_PUBLIC_.DIRECTORY_SEPARATOR.'uploads');
   define('_PATH_API_', 'api');
   define('_SEARCH_ENGINE_', false);
   define('_YEAR_', 2012);
   define('_PHONE_SEC_', '0326362384');
-  define('_EMAIL_', 'contact@bezannes-fsc.fr');
+  define('_EMAIL_', 'contact@fsc-bezannes.fr');
 
   if (false)
   {
@@ -21,6 +21,17 @@
     define('_ANALYTICS_FSC_', true);
     define('_ANALYTICS_GESTION_', true);
     define('_ANALYTICS_PREINSCRIPTION_', true);
+    define('_JQUERY_', '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');
+  }
+  else if (false)
+  {
+    define('_FSC_', '//localhost/public');
+    define('_GESTION_', '//localhost/gestion');
+    define('_ADMIN_', '//admin.fsc.localhost.local');
+    define('_PREINSCRIPTION_', '//localhost/preinscription');
+    define('_ANALYTICS_FSC_', false);
+    define('_ANALYTICS_GESTION_', false);
+    define('_ANALYTICS_PREINSCRIPTION_', false);
     define('_JQUERY_', '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');
   }
   else
@@ -35,7 +46,8 @@
     define('_JQUERY_', '//api.localhost.local/jquery/jquery-1.9.1.min.js');
   }
   
-  define('_STATIC_', _FSC_.'/'._PATH_UPLOADS_);
+  define('_STATIC_', _FSC_);
+  define('_UPLOADS_', _FSC_.'/'._PATH_UPLOADS_);
   define('_PUBLIC_API_', _FSC_.'/'._PATH_API_);
   define('_PRIVATE_API_', _GESTION_.'/'._PATH_API_);
 
