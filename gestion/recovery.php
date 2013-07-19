f310d4cdc0fa2787a71e4d2766ac0a798f4c11a3
nicolabricot <github@nicolabricot.com>
2013-07-20 01:20:21 +0200

    Change cookie name for fsc_gestion

--- gestion/recovery.php
+++ gestion/recovery.php
@@ -12,7 +12,7 @@ spl_autoload_extensions('.php');
 spl_autoload_register();
 //error_reporting (0);
 
-session_name('gestion');
+session_name('fsc_gestion');
 session_start();
 
 require '../config/config.php';
