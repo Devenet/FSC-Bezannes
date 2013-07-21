6235b5c79c91e3442dd1fc1cfe9dd6a4c1fc3afa
nicolabricot <github@nicolabricot.com>
2013-07-21 22:31:10 +0200

    Update preinscription view

--- gestion/pages/preinscription/view.php
+++ gestion/pages/preinscription/view.php
@@ -5,11 +5,20 @@ use lib\content\Display;
 <div class="row">
   <div class="span8">
     <div class="page-header" style="overflow:hidden; padding-bottom:5px;">
-      <h2 style="margin-bottom:0;"><?php echo $pre->name(); ?>
+      <h2 style="margin-bottom:0;"><?php echo Display::HtmlGender($pre->gender()), ' ', $pre->name(); ?>
       <div class="btn-group pull-right btn-small">
-        <a href="./?page=edit-member&amp;id=<?php echo $pre->id(); ?>" class="btn btn-small" title="Modifier le membre"><i class="icon-pencil"></i></a>
+        <a href="./?page=edit-preinscription&amp;id=<?php echo $pre->id(); ?>" class="btn btn-small" title="Modifier la préinscription"><i class="icon-pencil"></i></a>
       </div>
       </h2>
+    </div>
+
+    <p><i class="icon-gift"></i> <?php echo Display::Date($pre->date_birthday()), ' (', $pre->age(), ' ans)'; ?></p>
+
+  </div>
+
+  <div class="span3 offset1" style="margin-top:20px;">
+    <div class="well well-small">
+      <strong>Préinscription :</strong> #<?php echo $pre->id(); ?>
     </div>
   </div>
 </div>
