97d794db3b3ba3a9c0cff2fcabb8764a897e23d7
nicolabricot <github@nicolabricot.com>
2013-07-25 16:06:31 +0200

    Update preinscription display

--- gestion/pages/preinscription/controller.php
+++ gestion/pages/preinscription/controller.php
@@ -4,8 +4,11 @@ use lib\content\Page;
 use lib\users\UserInscription;
 use lib\preinscriptions\Preinscription;
 use lib\preinscriptions\Member;
+use lib\preinscriptions\Participant;
 use lib\content\Message;
 use lib\content\Display;
+use lib\activities\Activity;
+use lib\activities\Schedule;
 
 function quit() {
   header('Location: '. _GESTION_ .'/?page=preinscriptions');
@@ -29,6 +32,103 @@ if (isset($_GET['id']) && Member::isMember($_GET['id']+0)) {
       array('name' => $account->login(), 'url' => '?page=preinscriptions&amp;detail='.$account->id()),
       $pageInfos)
   );
+
+  // generation des buttons du membre en fonction de son status
+  function getButtonsMember() {
+    global $pre;
+    $result = '<a ';
+    if ($pre->status() ==  Preinscription::AWAITING)
+      $result .= 'href="'. _GESTION_ .'/?page=validate-preinscription&amp;id='. $pre->id() .'" title="Valider la préinscription"';
+    $result .= ' class="btn btn-small';
+    if ($pre->status() !=  Preinscription::AWAITING)
+      $result .= ' disabled';
+    $result .= '"><i class="icon-plus"></i></a>'. PHP_EOL;
+    $result .= '<div class="btn-group">';
+    $result .= '<a ';
+    if ($pre->status() ==  Preinscription::AWAITING)
+      $result .= 'href="'. _GESTION_ .'/?page=edit-preinscription&amp;id='. $pre->id() .'" title="Modifier la préinscription"';
+    $result .= ' class="btn btn-small';
+    if ($pre->status() !=  Preinscription::AWAITING)
+      $result .= ' disabled';
+    $result .= '"><i class="icon-pencil"></i></a>';
+    $result .= '<a href="#confirmRemoveInscription'. $pre->id() .'" role="button" data-toggle="modal" title="Supprimer la préinscription" class="btn btn-small"><i class="icon-trash"></i></a>';
+    $result .= '</div>';
+    return $result;
+
+  }
+  // geneation des boutons en fonction du status (préinscription & participation)
+  function getButtonsParticipant($p) {
+    global $pre;
+    switch ($pre->status()) {
+      case Preinscription::AWAITING:
+        return 'préinscription non validée';
+        break;
+
+      case Preinscription::INCOMPLETE:
+      case Preinscription::VALIDATED:
+        if ($p->status() == Preinscription::AWAITING)
+          return '<a href="#confirmBoxP'. $p->id() .'"  role="button" data-toggle="modal" class="normal"><i class="icon-trash"></i></a>';
+        return 'nop';
+        break;
+
+      case Preinscription::REJECTED:
+      default:
+        return 'not allowed';
+        break;
+    }
+  }
+
+
+  // Activités
+  $count_activites = Participant::countActivities($pre->id());
+  $plural_count_activities = $count_activites > 1 ? 's' : '';
+  
+  $display_participatitions = '';
+  if ($count_activites >= 1) {
+    $display_participatitions = '<table class="table table-striped table-go"><thead>
+      <tr>
+        <th>Activité</th>
+        <th>Horaire</th>
+        <th class="center">Statut</th>
+      </tr>
+      </thead><tbody>
+    ';
+    foreach (Participant::Activities($pre->id()) as $p) {
+      $a = new Activity($p->activity());
+      if (!$a->aggregate())
+        $s = new Schedule($p->schedule());
+      $horaire = isset($s) && $a->aggregate() == 0 ? Display::Day($s->day()).' &rsaquo; '. $s->time_begin() .' à '. $s->time_end() . ($s->more() != NULL ? '&nbsp; &nbsp;('.$s->more().')' : '') : '<em>Pratique libre</em>';
+      $horaire = isset($s) && $s->description() != NULL ? $s->description() : $horaire;
+      $display_participatitions .= '
+        <tr>
+          <td class="go"><a href="'. _FSC_ .'/activite/'. $a->url() .'" rel="external">'. $a->name() .'</a> <span class="external-link"><i class="icon-external-link"></i></span></td>
+          <td>'. $horaire .'</td>
+          <td class="center status">'. Preinscription::StatusTooltip($p->status()) .'</td>
+          <td class="center">'. getButtonsParticipant($p) .'</td>
+        </tr>
+      ';
+    }
+    $display_participatitions .= '</tbody></table>';
+    $_SCRIPT[] = '<script>$(function(){ $(\'table td.status span\').tooltip(); });</script>';
+
+    foreach (Participant::Activities($pre->id()) as $p)
+      $display_participatitions .= '
+      <div id="confirmBoxP'. $p->id() .'" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="ConfirmDelParticipant'. $p->id() .'" aria-hidden="true">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h3 id="ConfirmDelParticipant'. $p->id() .'">'. $pre->name() .'</h3>
+        </div>
+        <div class="modal-body">
+          <p class="text-error">Êtes-vous sûr de vouloir désinscrire ce membre de l’activité ?</p>
+        </div>
+        <div class="modal-footer">
+          <a class="btn" data-dismiss="modal" aria-hidden="true">Annuler</a>
+          <a href="'. _PREINSCRIPTION_ .'/remove-activity/'. $p->id() .'" class="btn btn-danger">Confirmer</a>
+        </div>
+      </div>
+      ';
+  }
+
 
   $_SCRIPT[] = '<script>$(function(){ $(\'.status-tooltip span\').tooltip({"placement": "right"}); });</script>';
 
--- gestion/pages/preinscription/view.php
+++ gestion/pages/preinscription/view.php
@@ -1,24 +1,101 @@
 <?php
 use lib\content\Display;
+use lib\preinscriptions\Preinscription;
 ?>
 
 <div class="row">
   <div class="span8">
     <div class="page-header" style="overflow:hidden; padding-bottom:5px;">
-      <h2 style="margin-bottom:0;"><?php echo Display::HtmlGender($pre->gender()), ' ', $pre->name(); ?>
-      <div class="btn-group pull-right btn-small">
-        <a href="./?page=edit-preinscription&amp;id=<?php echo $pre->id(); ?>" class="btn btn-small" title="Modifier la préinscription"><i class="icon-pencil"></i></a>
+      <h2 style="margin-bottom:0;"><?php echo $pre->name(); ?> <small class="status-tooltip" style="vertical-align:super;"><?php echo Preinscription::StatusTooltip($pre->status()); ?></small>
+      <div class="pull-right btn-small">
+        <?php echo getButtonsMember(); ?>
       </div>
       </h2>
     </div>
 
-    <p><i class="icon-gift"></i> <?php echo Display::Date($pre->date_birthday()), ' (', $pre->age(), ' ans)'; ?></p>
+    <div class="row-fluid" style="margin-top:-20px;">
+      <div class="span5 muted">
+        <?php echo Display::HtmlGender($pre->gender()), ' ', $pre->name(); ?>
+      </div>
+      <div class="span3">
+        <?php echo Display::Date($pre->date_birthday()), ' (', $pre->age(), ' ans)'; ?>
+      </div>
+      <div class="span3 offset1 muted">
+        <?php echo $pre->minor() ? 'Jeune' : 'Adulte'; ?> &middot; <?php echo $pre->bezannais() ? 'Bezannais' : 'Extérieur'; ?> 
+      </div>
+    </div>
 
+    <div class="row-fluid espace-top">
+      <?php if (!$pre->minor() || $pre->address_different()) : ?>
+        <div class="span4">
+          <h4>Coordonnées</h4>
+          <address>
+            <?php echo $pre->address(); ?>
+          </address>
+        </div>
+      <?php endif; ?>
+
+      <div class="span4">
+        <h4>Contact</h4>
+          <p><i class="icon-phone"></i> <?php echo Display::Phone($pre->phone()); ?>
+          <?php echo ($pre->mobile() != NULL ? '<br /><i class="icon-phone"></i> '. Display::Phone($pre->mobile()) : NULL); ?>
+          <br /><i class="icon-envelope-alt"></i> <?php echo Display::Email($pre->email()); ?></p>
+      </div>
+
+      <?php if ($pre->minor()): ?>
+        <div class="span4">
+          <h4>Reponsable</h4>
+          <p><?php echo Display::HtmlGender($respo->gender()), ' ', $respo->name(); ?> [<a href="<?php echo _GESTION_; ?>/?page=preinscription&amp;id=<?php echo $respo->id(); ?>">#<?php echo $respo->id(); ?></a>]
+          <br /><span class="muted"><?php echo $respo->address_zip_code(), ' ', $respo->address_town(); ?></span></p>
+        </div>
+      <?php
+      endif;
+      if ($pre->countResponsabilities() > 0) {
+        echo '<div class="span4">', '<h4>Responsabilité</h4>';
+        $minors = $pre->Responsabilities();
+        echo '<ul>';
+        foreach ($minors as $minor)
+          echo '<li>', $minor->name(), ' [<a href="'. _GESTION_ .'/?page=preinscription&amp;id=', $minor->id(), '" >#', $minor->id(), '</a>]</li>';
+        echo '</ul>', '</div>';
+      }
+      ?>  
+    </div>
   </div>
 
   <div class="span3 offset1" style="margin-top:20px;">
     <div class="well well-small">
       <strong>Préinscription :</strong> #<?php echo $pre->id(); ?>
     </div>
+
+    <div class="alert <?php echo ($pre->adherent() ? 'alert-success' : ''); ?>">
+      <strong><?php echo ($pre->adherent() ? 'Pré-adhérent' : 'Non-adherent'); ?></strong>
+    </div>
   </div>
+</div>
+
+<div class="row espace-top">
+  <div class="span8 espace-bottom">
+    <hr />
+  </div>
+  <div class="span12">
+    <h4>Activité<?php echo $plural_count_activities; ?> en préinscription</h4>
+  </div>
+  <div class="span12">
+    <?php echo $display_participatitions; ?>
+  </div>
+</div>
+
+
+<div id="confirmRemoveInscription<?php echo $pre->id(); ?>" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="ConfirmDelInscription<?php echo $pre->id(); ?>" aria-hidden="true">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+<h3 id="ConfirmDelInscription<?php echo $pre->id(); ?>"><?php echo $pre->name(); ?></h3>
+</div>
+<div class="modal-body">
+<p class="text-error">Êtes-vous sûr de vouloir supprimer cette préinscription ?</p>
+</div>
+<div class="modal-footer">
+<a class="btn" data-dismiss="modal" aria-hidden="true">Annuler</a>
+<a href="./?page=preinscription&amp;id=<?php echo $pre->id(); ?>&amp;action=delete" class="btn btn-danger">Confirmer</a>
+</div>
 </div>
