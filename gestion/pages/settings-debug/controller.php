e24fa511150026773764b11ae85be8ba75eebda4
nicolabricot <github@nicolabricot.com>
2013-09-01 17:53:45 +0200

    Debug page in settings
    - constants (old settings page)
    - db state
    - host links

---
+++ gestion/pages/settings-debug/controller.php
@@ -0,0 +1,63 @@
+<?php
+
+use lib\content\Page;
+use lib\users\UserAdmin;
+use lib\content\Display;
+use lib\db\SQL;
+
+$pageInfos = array(
+	'name' => 'Configuration',
+	'url' => '/?page=configuration'
+);
+$page = new Page($pageInfos['name'], $pageInfos['url'], array($pageInfos));
+
+$configs = array (
+	'_SEARCH_ENGINE_',
+	'_YEAR_',
+	'_PHONE_SEC_',
+	'_EMAIL_',
+	'_FSC_',
+	'_GESTION_',
+	'_PREINSCRIPTION_',
+	'_ANALYTICS_FSC_',
+	'_ANALYTICS_GESTION_',
+	'_ANALYTICS_PREINSCRIPTION_',
+	'_PATH_PUBLIC_',
+	'_PATH_GESTION_',
+	'_PATH_PREINSCRIPTION_',
+	'_PATH_UPLOADS_',
+	'_PATH_UPLOADS_FULL_',
+	'_PATH_API_',
+	'_STATIC_',
+	'_UPLOADS_',
+	'_PUBLIC_API_',
+	'_PRIVATE_API_',
+);
+$display_constants = '';
+foreach ($configs as $c)
+		$display_constants .= '<tr style="font-family: monospace;"><td><code>'. $c .'</code></td><td>'. (is_bool(constant($c)) ? (constant($c) ? '<span style="color:rgb(70, 136, 71);">true</span>' : '<span style="color:rgb(185, 74, 72);">false</span>') : constant($c)) .'</td></tr>';
+
+$bases = array(
+	'activities',
+	'history_admin', 
+	'members',
+	'members_inscription',
+	'participants', 
+	'participants_inscription', 
+	'payments_advantages', 
+	'payments_transactions', 
+	'referents',
+	'schedules', 
+	'users_admin', 
+	'users_inscription', 
+	'users_recover_passwords'
+);
+$display_db = '';
+foreach ($bases as $db) {
+	$display_db .= '<tr><td>fsc_<strong>'. $db .'</strong></td><td class="center">';
+	$query = SQL::sql()->query('SELECT COUNT(id) AS total FROM fsc_'.$db);
+	$data = $query->fetch();
+	$display_db .= $data['total']. '</td></tr>';
+}
+
+?>
---
+++ gestion/pages/settings-debug/view.php
@@ -0,0 +1,56 @@
+<div class="row">
+	<div class="span12">
+		<div class="page-header">
+			<h2 style="margin-bottom: 0;">Configuration</h2>
+		</div>
+	</div>
+</div>
+
+<ul class="nav nav-tabs">
+	<li><a href="?page=settings">Paramètres</a></li>
+	<li><a href="?page=settings-prices">Cotisations</a></li>
+	<li class="pull-right active"><a href="?page=settings-debug">Déboguage</a/></li>
+</ul>
+
+<div class="tabbable tabs-left">
+	<ul class="nav nav-tabs">
+		<li class="active"><a href="#tab-constants" data-toggle="tab">Constantes</a></li>
+		<li><a href="#tab-db" data-toggle="tab">État de la BDD</a></li>
+		<li><a href="#tab-ovh" data-toggle="tab">OVH</a></li>
+	</ul>
+	<div class="tab-content espace-small-top" style="padding-left:20px;">
+		<div class="tab-pane active" id="tab-constants">
+			<table class="table table-striped table-hover">
+				<thead>
+					<tr>
+						<th>Constante</th>
+						<th>Valeur</th>
+					</tr>
+				</thead>
+				<?php echo $display_constants; ?>
+			</table>
+		</div>
+		
+		<div class="tab-pane" id="tab-db">
+			<table class="table table-striped table-hover" style="max-width: 300px;">
+				<thead>
+					<tr>
+						<th>Nom de la table</th>
+						<th class="center">Valeur</th>
+					</tr>
+				</thead>
+				<?php echo $display_db; ?>
+			</table>
+		</div>
+
+		<div class="tab-pane" id="tab-ovh">
+			<ul>
+				<li><a href="https://www.ovh.com/managerv3/" rel="external">Manager <i class="icon-external-link"></i></a></li>
+				<li><a href="https://phpmyadmin.ovh.net/" rel="external">phpMyAdmin <i class="icon-external-link"></i></a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+
+<div class="clearfix"></div>
